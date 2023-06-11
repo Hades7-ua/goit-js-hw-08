9dccc6183d6d2a6232f5ace8c605ab9513fe1270
Vitalii Tkachov <tkachov0703@gmail.com>
2023-06-11 22:14:52 +0300

    correction of mistakes

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,7 @@
 import throttle from 'lodash.throttle ';
 
 const FEEDBACK_STORAGE_KEY = 'feedback-form-state';
-let formData = {};
+let formData = JSON.parse(localStorage.getItem(FEEDBACK_STORAGE_KEY)) || {};
 
 const form = document.querySelector('.feedback-form');
 
@@ -15,12 +15,33 @@ function onFormInput(e) {
   localStorage.setItem(FEEDBACK_STORAGE_KEY, JSON.stringify(formData));
 }
 
+// function onFormSubmit(e) {
+//   e.preventDefault();
+//   e.currentTarget.reset();
+//   localStorage.removeItem(FEEDBACK_STORAGE_KEY);
+//   console.log(formData);
+//   formData = {};
+// }
+
 function onFormSubmit(e) {
   e.preventDefault();
+
+  //   const formData = {};
+
+  const formElements = e.currentTarget.elements;
+  const email = formElements.email;
+  const message = formElements.message;
+
+  if (email.value === '' || message.value === '') {
+    alert('Заповніть будь ласка всі поля');
+  } else {
+    formData.email = email.value;
+    formData.message = message.value;
+  }
+
+  console.log(JSON.parse(localStorage.getItem(FEEDBACK_STORAGE_KEY)));
+
   e.currentTarget.reset();
-  localStorage.removeItem(FEEDBACK_STORAGE_KEY);
-  console.log(formData);
-  formData = {};
 }
 
 function pageReload() {
